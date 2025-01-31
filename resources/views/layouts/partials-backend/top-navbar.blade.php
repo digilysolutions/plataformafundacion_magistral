a124353a73dcec9f56bb7cf89807b34327b270e9
digilysolutions <digilysolutions@gmail.com>
2025-01-31 01:15:44 -0500

    Update top-navbar.blade.php

--- resources/views/layouts/partials-backend/top-navbar.blade.php
+++ resources/views/layouts/partials-backend/top-navbar.blade.php
@@ -3,9 +3,9 @@
         <nav class="navbar navbar-expand-lg navbar-light p-0">
             <div class="iq-navbar-logo d-flex align-items-center justify-content-between">
                 <i class="ri-menu-line wrapper-menu"></i>
-                <a href="backend/index.html" class="header-logo">
-                    <img src="" class="img-fluid rounded-normal" alt="logo">
-                    <h5 class="logo-title ml-3">Fundación Magistral</h5>
+                <a href="" class="header-logo">
+                    <img src="{{asset('img/23.png')}}" class="img-fluid rounded-normal" alt="logo">
+                    <h5 class="logo-title ml-3">Plataforma</h5>
 
                 </a>
             </div>
