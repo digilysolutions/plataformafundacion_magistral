703a7a0d7a3869411a5ee48c408d56ff898da998
digilysolutions <digilysolutions@gmail.com>
2025-02-21 12:49:37 -0500

    Update views tutor

--- resources/views/tutor/create.blade.php
+++ resources/views/tutor/create.blade.php
@@ -8,7 +8,6 @@
     <section class="content container-fluid">
         <div class="row">
             <div class="col-md-12">
-
                 <div class="card card-default">
                     <div class="card-header">
                         <span class="card-title">{{ __('Crear') }} Tutor</span>
@@ -18,7 +17,7 @@
                     </div>
 
                     <div class="card-body bg-white">
-                        <form method="POST" action="{{ route('tutors.store') }}"  role="form" enctype="multipart/form-data">
+                        <form method="POST" action="{{ route('tutors.store') }}"  role="form" enctype="multipart/form-data" data-toggle="validator">
                             @csrf
 
                             @include('tutor.form')
--- resources/views/tutor/form.blade.php
+++ resources/views/tutor/form.blade.php
@@ -1,30 +1,29 @@
 <div class="row padding-1 p-1">
-    <div class="col-md-12">
 
-
-        <div class="form-group mb-2 mb20">
-            <label for="name" class="form-label">{{ __('Nombre') }}</label>
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $tutor?->name) }}" id="name" placeholder="Nombre">
+        <div class="form-group col-md-6">
+            <label for="name" class="form-label">{{ __('Nombre') }} *</label>
+            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" value="{{ old('name', $tutor?->name) }}" id="name" placeholder="Entra el nombre del tutor" required>
+            <div class="help-block with-errors"></div>
             {!! $errors->first('name', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
-        <div class="form-group mb-2 mb20">
+        <div class="form-group col-md-6">
             <label for="lastname" class="form-label">{{ __('Apellidos') }}</label>
             <input type="text" name="lastname" class="form-control @error('lastname') is-invalid @enderror" value="{{ old('lastname', $tutor?->lastname) }}" id="lastname" placeholder="Apellidos">
             {!! $errors->first('lastname', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
-        <div class="form-group mb-2 mb20">
+        <div class="form-group col-md-6">
             <label for="email" class="form-label">{{ __('Correo') }}</label>
             <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" value="{{ old('email', $tutor?->email) }}" id="email" placeholder="Correo">
             {!! $errors->first('email', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
-        <div class="form-group mb-2 mb20">
+        <div class="form-group col-md-6">
             <label for="phone" class="form-label">{{ __('Teléfono') }}</label>
             <input type="text" name="phone" class="form-control @error('phone') is-invalid @enderror" value="{{ old('phone', $tutor?->phone) }}" id="phone" placeholder="Teléfono">
             {!! $errors->first('phone', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
         </div>
-        <div class="form-group mb-2 mb20">
-            <label for="address" class="form-label">{{ __('Centro de Estudio') }}</label>
-            <select id="studycenters_id" name="studycenters_id" class="form-control">
+        <div class="form-group  col-md-12">
+            <label for="address" class="form-label">{{ __('Centro de Estudio') }} *</label>
+            <select id="studycenters_id" name="studycenters_id" class="form-control" required>
                 @foreach ($studyCenters as $studyCenter)
                     <option value="{{ $studyCenter->id }}" @if ($studyCenter->id==$tutor->studycenters_id) select
 
@@ -33,21 +32,22 @@
                 @endforeach
 
             </select>
+            <div class="help-block with-errors"></div>
         </div>
-        <div class="form-group mb-2 mb20">
-            <label for="address" class="form-label">{{ __('Especialidad') }}</label>
-            <select id="specialty_id" name="specialty_id" class="form-control">
-                @foreach ($specialties as $specialty)
+
+        <div class="form-group col-md-12">
+            <label class="form-label" for="terms">Selecciona las especialidades *</label>
+
+                <select id="specialty_id" class="form-control" name="specialty_id[]" multiple="" required>
+                    @foreach ($specialties as $specialty)
                     <option value="{{ $specialty->id }}" @if ($specialty->id==$tutor->specialty_id) select
 
                     @endif>
                         {{ $specialty->name }}</option>
                 @endforeach
-
-            </select>
+                </select>
+                <div class="help-block with-errors"></div>
         </div>
-
-    </div>
     <div class="col-md-12 mt20 mt-2">
         <button type="submit" class="btn btn-primary">{{ __('Enviar') }}</button>
     </div>
