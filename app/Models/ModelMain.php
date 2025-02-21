9e71de45bafe993795d94c75156692637d9f0f9a
digilysolutions <digilysolutions@gmail.com>
2025-02-21 14:42:32 -0500

    Update Generador de codigo de seguimiento
    Gera codigo para las dos primeras letras de un nombre y  un numero consecutivo

--- app/Models/ModelMain.php
+++ app/Models/ModelMain.php
@@ -10,12 +10,28 @@ class ModelMain extends Model
     protected $keyType = 'string';
     public $incrementing = false;
 
+
     protected static function boot()
     {
         parent::boot();
 
         static::creating(function ($model) {
-            $model->{$model->getKeyName()} = (string) Uuid::uuid4();
+            // Asegúrate de que el nombre está presente en los atributos
+            if (isset($model->name)) {
+                // Obtén las primeras 2 letras del nombre en mayúsculas
+                $prefix = strtoupper(substr($model->name, 0, 2));
+                // Obtén el siguiente número consecutivo basado en el prefijo
+                $numeroConsecutivo = $model->getNextConsecutiveNumber($prefix);
+
+                // Asigna el valor de la clave primaria
+                $model->{$model->getKeyName()} = $prefix . str_pad($numeroConsecutivo, 4, '0', STR_PAD_LEFT);
+            }
         });
+    }
+
+    private function getNextConsecutiveNumber($prefix)
+    {
+        // Cuenta los registros existentes que comienzan con el prefijo
+        return self::where('id', 'LIKE', "$prefix%")->count() + 1; // Suponiendo que tu clave primaria es 'id'
     }
 }
