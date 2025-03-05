ffee61f239de03d3cb69665511c9cd1f888a7101
digilysolutions <digilysolutions@gmail.com>
2025-03-05 10:50:30 -0500

    Update login.blade.php
    Se modificala seccion para mostrar los mensajes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,16 +70,18 @@
         <section class="login-content">
             <div class="container">
                 <div class="row align-items-center justify-content-center height-self-center">
+
                     <div class="col-lg-8">
                         <div class="card auth-card">
-                            <div class="card-body p-0">
-                                <div class="d-flex align-items-center auth-content">
-                                    <div class="col-lg-7 align-self-center">
-                                        @if (session('error'))
+                            @if (session('error'))
                                             <div class="alert alert-danger">
                                                 {{ session('error') }}
                                             </div>
                                         @endif
+                            <div class="card-body p-0">
+                                <div class="d-flex align-items-center auth-content">
+                                    <div class="col-lg-7 align-self-center">
+
                                         <div class="p-3">
                                             <h2 class="mb-2">Iniciar Sesión</h2>
                                             <p>Inicia sesión para mantenerte conectado con:</p>
