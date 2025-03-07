01e7395e5c3719fb4c646841ab0915d83c1f672e
digilysolutions <digilysolutions@gmail.com>
2025-03-07 12:09:20 -0500

    Update DatabaseSeeder.php
    crear usuario por defecto. Creando persona y estudiante

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\MembershipFeature;
+use App\Models\Person;
+use App\Models\Student;
 use App\Models\StudyCenter;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -16,6 +18,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call(MembershipStatusSeeder::class);
+        $this->call(MembershipSeeder::class);
         // User::factory(10)->create();
         $roles = [
             'Centro Educativo',
@@ -31,69 +35,86 @@ class DatabaseSeeder extends Seeder
         }
 
 
-       User::factory()->create([
+        User::factory()->create([
             'name' => 'Centro educativo',
             'email' => 'centroeducativo@gmail.com',
-            'password'=>'1234',
-            'role'=>'Centro Educativo',
-            'roleid'=>1
+            'password' => '1234',
+            'role' => 'Centro Educativo',
+            'roleid' => 1
         ]);
         $person_studyCenter = [
             [
                 'name' => 'Responsable Center',
-                'lastname' => 'ytrpiloto@gmail.com',
-                'email' => 'Centro educativo',
+                'lastname' => 'Centro educativo',
+                'email' => 'ytrpiloto@gmail.com',
                 'user_id' => 1,
                 'activated' => true,
             ]
         ];
 
-      User::factory()->create([
+        User::factory()->create([
             'name' => 'Test User',
             'email' => 'estudiante@gmail.com',
-            'password'=>'1234',
-            'role'=>'Estudiante',
-            'roleid'=>2
+            'password' => '1234',
+            'role' => 'Estudiante',
+            'roleid' => 2
         ]);
 
         User::factory()->create([
             'name' => 'Tutor',
             'email' => 'tutor@gmail.com',
-            'password'=>'1234',
-            'role'=>'Tutor',
-            'roleid'=>3
+            'password' => '1234',
+            'role' => 'Tutor',
+            'roleid' => 3
         ]);
 
         User::factory()->create([
             'name' => 'Validador',
             'email' => 'validador@gmail.com',
-            'password'=>'1234',
-            'role'=>'Validador',
-            'roleid'=>4
+            'password' => '1234',
+            'role' => 'Validador',
+            'roleid' => 4
         ]);
 
         User::factory()->create([
             'name' => 'Administrador',
             'email' => 'admin@gmail.com',
-            'role'=>'Administrador',
-            'password'=>'1234',
-            'roleid'=>5
+            'role' => 'Administrador',
+            'password' => '1234',
+            'roleid' => 5
         ]);
 
-        User::factory()->create([
+        $user = User::factory()->create([
             'name' => 'Usuario',
             'email' => 'usuario@gmail.com',
-            'role'=>'Usuario',
-            'password'=>'1234',
-            'roleid'=>6
+            'role' => 'Usuario',
+            'password' => '1234',
+            'roleid' => 6
         ]);
+        $person = Person::create(
+            [
+                'name' => 'Usuario ',
+                'lastname' => 'Normal',
+                'email' => 'usuario@gmail.com',
+                'phone' => '58205454',
+                'user_id' =>  $user->id,
+            ]
+        );
+        $student = Student::create(
+            [
+                'name' =>  $person->name,
+                'people_id'=>$person->id,
+                'course'=>'Curso 1',
+                'membership_id'=>'BA0001'
+
+            ]
+        );
 
         $this->call(CountrySeeder::class);
         $this->call(RegionalsSeeder::class);
         $this->call(DistrictsSeeder::class);
         $this->call(SpecialtiesSeeder::class);
-        $this->call(MembershipStatusSeeder::class);
-        $this->call(MembershipSeeder::class);
+
         $this->call(FeatureSeeder::class);
         $this->call(StyCenterSeeder::class);
     }
