f175a5457d6a757a4dd883241ff719049702cdd4
digilysolutions <digilysolutions@gmail.com>
2025-03-07 10:18:42 -0500

    update Dashboard
    Mensaje de bienvenida para saber que entre al dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -3,5 +3,12 @@
    PLataforma Fundación Magistral
 @endsection
 @section('content-admin')
-    Usuario
+<div class="col-lg-12">
+    <div class="card card-transparent card-block card-stretch card-height border-none">
+        <div class="card-body p-3 mt-lg-2 mt-3">
+            <h3 class="mb-3">¡Bienvenido al Dashboard de Usuario!</h3>
+            <p class="mb-0 mr-4">Aquí podrás visualizar  ¡Explora y aprende!</p>
+        </div>
+    </div>
+</div>
 @endsection
