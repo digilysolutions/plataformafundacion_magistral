79ff8d9418d638264a7bc9ed8f37f19a87c7a372
digilysolutions <digilysolutions@gmail.com>
2025-03-07 00:47:33 -0500

    Update 2025_02_11_041654_create_students_table.php

--- database/migrations/2025_02_11_041654_create_students_table.php
+++ database/migrations/2025_02_11_041654_create_students_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->boolean('activated')->default(false);
             $table->string('name')->nullable();
+
             $table->uuid('people_id');
             $table->foreign('people_id')->references('id')->on('people')->onDelete('cascade');
             $table->string('course')->nullable();
