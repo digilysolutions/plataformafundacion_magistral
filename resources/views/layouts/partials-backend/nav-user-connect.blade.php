7b4b9735c8e4369084ed420069ca5c0008558f18
digilysolutions <digilysolutions@gmail.com>
2025-03-11 10:28:02 -0400

    Update nav-user-connect.blade.php

--- resources/views/layouts/partials-backend/nav-user-connect.blade.php
+++ resources/views/layouts/partials-backend/nav-user-connect.blade.php
@@ -6,4 +6,11 @@
         <i class="ri-pie-chart-line"></i> Online: {{ $activeUsersCount }}
     </a>
 </li>
+@else
+<li class="nav-item nav-icon dropdown">
+    <a href="#" class="search-toggle dropdown-toggle" id="dropdownMenuButton2"
+        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <i class="ri-pie-chart-line"></i> Tiempo en Plataforma: 20min
+    </a>
+</li>
 @endif
