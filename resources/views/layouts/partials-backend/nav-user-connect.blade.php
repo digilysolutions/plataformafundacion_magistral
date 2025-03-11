0e1a193d90392bb50679cc27c976ded76700ad04
digilysolutions <digilysolutions@gmail.com>
2025-03-11 12:05:34 -0400

    Update nav-user-connect.blade.php

--- resources/views/layouts/partials-backend/nav-user-connect.blade.php
+++ resources/views/layouts/partials-backend/nav-user-connect.blade.php
@@ -3,7 +3,7 @@
 <li class="nav-item nav-icon dropdown">
     <a href="#" class="search-toggle dropdown-toggle" id="dropdownMenuButton2"
         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        <i class="ri-pie-chart-line"></i> cantidad de usuarios online : {{ $activeUsersCount }}
+        <i class="ri-pie-chart-line"></i> Cantidad de usuarios online : {{ $activeUsersCount }}
     </a>
 </li>
 @elseif ($user->role == 'Usuario')
