877ba7fffbcd795ee12aaf2674f48e59b1123621
digilysolutions <digilysolutions@gmail.com>
2025-03-11 09:31:03 -0400

    Update dashboard.blade.php

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -10,5 +10,85 @@
             <p class="mb-0 mr-4">Aquí podrás visualizar  ¡Explora y aprende!</p>
         </div>
     </div>
+    <div class="col-lg-12">
+        <div class="row">
+            <div class="col-lg-3 col-md-4">
+                <div class="card card-block card-stretch card-height">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center mb-4 card-total-sale">
+                            <div class="icon iq-icon-box-2 bg-info-light">
+                                <img src="{{ asset('img/47.jpg') }}" class="img-fluid" alt="image">
+                            </div>
+                            <div>
+                                <p class="mb-2">Items Resueltos</p>
+                                <h4>0</h4>
+                            </div>
+                        </div>
+                        <div class="iq-progress-bar mt-2">
+                            <span class="bg-info iq-progress progress-1" data-percent="85">
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4">
+                <div class="card card-block card-stretch card-height">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center mb-4 card-total-sale">
+                            <div class="icon iq-icon-box-2 bg-danger-light">
+                                <img src="{{ asset('img/a4.png') }}" class="img-fluid" alt="image">
+                            </div>
+                            <div>
+                                <p class="mb-2">Items no Resueltos</p>
+                                <h4>0</h4>
+                            </div>
+                        </div>
+                        <div class="iq-progress-bar mt-2">
+                            <span class="bg-danger iq-progress progress-1" data-percent="70">
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4">
+                <div class="card card-block card-stretch card-height">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center mb-4 card-total-sale">
+                            <div class="icon iq-icon-box-2 bg-success-light">
+                                <img src="{{ asset('img/3.png') }}" class="img-fluid" alt="image">
+                            </div>
+                            <div>
+                                <p class="mb-2">Exámenes Resueltos</p>
+                                <h4>0</h4>
+                            </div>
+                        </div>
+                        <div class="iq-progress-bar mt-2">
+                            <span class="bg-success iq-progress progress-1" data-percent="75">
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-4">
+                <div class="card card-block card-stretch card-height">
+                    <div class="card-body">
+                        <div class="d-flex align-items-center mb-4 card-total-sale">
+                            <div class="icon iq-icon-box-2 bg-info">
+                                <img src="{{ asset('img/02.png') }}" class="img-fluid" alt="image">
+                            </div>
+                            <div>
+                                <p class="mb-2">Exámenes no Resueltos</p>
+                                <h4>0</h4>
+                            </div>
+                        </div>
+                        <div class="iq-progress-bar mt-2">
+                            <span class="bg-success iq-progress progress-1" data-percent="75">
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
