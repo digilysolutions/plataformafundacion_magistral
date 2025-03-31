a93d0af45f412d23c0ea935ee6bf177324fdd573
digilysolutions <digilysolutions@gmail.com>
2025-03-31 15:23:20 -0400

    Update DistrictsSeeder.php

--- database/seeders/DistrictsSeeder.php
+++ database/seeders/DistrictsSeeder.php
@@ -3,11 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\District;
-use App\Models\Regional;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class DistrictsSeeder extends Seeder
 {
@@ -18,6 +14,7 @@ class DistrictsSeeder extends Seeder
     {
 
         $districts = [
+            //Distritos de  BARAHONA                                                                                                                                                                                   
             [
                 'name' => 'PEDERNALES',
                 'activated' => true,
@@ -48,6 +45,82 @@ class DistrictsSeeder extends Seeder
                 'code' => '0105',
                 'regional_id' => 'BA0001',
             ],
+             //Distritos de SAN JUAN DE LA MAGUANA 
+             [
+                'name' => 'COMENDADOR',
+                'activated' => true,
+                'code' => '0201',
+                'regional_id' => 'SA0001',
+            ],
+            [
+                'name' => 'PEDRO SANTANA',
+                'activated' => true,
+                'code' => '0202',
+                'regional_id' => 'SA0001',
+            ],
+            [
+                'name' => 'LAS MATAS DE FARFAN',
+                'activated' => true,
+                'code' => '0203',
+                'regional_id' => 'SA0001',
+            ],
+            [
+                'name' => 'EL CERCADO',
+                'activated' => true,
+                'code' => '0204',
+                'regional_id' => 'SA0001',
+            ],
+            [
+                'name' => 'SAN JUAN ESTE',
+                'activated' => true,
+                'code' => '0205',
+                'regional_id' => 'SA0001',
+            ],
+            [
+                'name' => 'SAN JUAN OESTE',
+                'activated' => true,
+                'code' => '0206',
+                'regional_id' => 'SA0001',
+            ],
+            [
+                'name' => 'HONDO VALLE',
+                'activated' => true,
+                'code' => '0207',
+                'regional_id' => 'SA0001',
+            ],
+
+             //Distritos de AZUA 
+             [
+                'name' => 'AZUA',
+                'activated' => true,
+                'code' => '0301',
+                'regional_id' => 'AZ0001',
+            ],
+            [
+                'name' => 'PADRE DE LAS CASAS',
+                'activated' => true,
+                'code' => '0302',
+                'regional_id' => 'AZ0001',
+            ],
+            [
+                'name' => 'SAN JOSE DE OCOA',
+                'activated' => true,
+                'code' => '0303',
+                'regional_id' => 'AZ0001',
+            ],
+            [
+                'name' => 'BANI',
+                'activated' => true,
+                'code' => '0304',
+                'regional_id' => 'AZ0001',
+            ],
+            [
+                'name' => 'NIZAO',
+                'activated' => true,
+                'code' => '0305',
+                'regional_id' => 'AZ0001',
+            ],
+
             // Agrega más distritos según sea necesario
         ];
         foreach ($districts as $district) {
