d918d564358855335a8015f74527bcfd810b4b47
digilysolutions <digilysolutions@gmail.com>
2025-04-13 17:14:52 -0400

    Update 2025_02_10_203321_create_levels_table.php

--- database/migrations/2025_02_10_203321_create_levels_table.php
+++ database/migrations/2025_02_10_203321_create_levels_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('levels', function (Blueprint $table) {
             $table->uuid('id')->primary();
-            $table->integer('id')->nullable();
             $table->boolean('activated')->default(false);
             $table->string('name');
             $table->string('description')->nullable();
