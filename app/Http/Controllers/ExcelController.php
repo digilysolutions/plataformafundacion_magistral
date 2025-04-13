8f34de49c92fa00ebd23ab7a3cc1ad8c4947f990
digilysolutions <digilysolutions@gmail.com>
2025-04-13 09:02:53 -0400

    Update ExcelController.php

--- app/Http/Controllers/ExcelController.php
+++ app/Http/Controllers/ExcelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Imports\EstudiantesImport; // Asegúrate de crear esta clase de importación
 use App\Imports\RegionalImport;
+use App\Imports\StudentsImport;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -14,7 +15,6 @@ class ExcelController extends Controller
     {
         return view('import');
     }
-
     public function import(Request $request)
     {
         $request->validate([
@@ -22,6 +22,24 @@ class ExcelController extends Controller
         ]);
 
         Excel::import(new RegionalImport, $request->file('import_file'));
+
+        return back()->with('success', 'Datos importados exitosamente.');
+    }
+
+
+    //-------------Estudiantes---------
+    ///Vista para la carga Inicial de los estudiantes
+    public function importViewStudents()
+    {
+        return view('importStudents');
+    } //Importar estudiantes
+    public function importStudents(Request $request)
+    {
+        $request->validate([
+            'import_file' => 'required|mimes:xlsx,xls',
+        ]);
+
+        Excel::import(new StudentsImport, $request->file('import_file'));
 
         return back()->with('success', 'Datos importados exitosamente.');
     }
