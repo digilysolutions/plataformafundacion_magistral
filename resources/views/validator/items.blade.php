aa379ad6f7f49406f5b6fed854c4f926fe4784ec
digilysolutions <digilysolutions@gmail.com>
2025-04-25 14:33:37 -0400

    Update items.blade.php

--- resources/views/validator/items.blade.php
+++ resources/views/validator/items.blade.php
@@ -13,7 +13,7 @@
                     <div style="display: flex; justify-content: space-between; align-items: center;">
 
                         <span id="card_title">
-                            {{ __('Usuario General') }}
+                            {{ __('Items') }}
                         </span>
 
                         <div class="float-right">
