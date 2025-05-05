3306ebe9c83e87e46cffb7704751a2f2a2bd4471
digilysolutions <digilysolutions@gmail.com>
2025-05-05 15:30:32 -0400

    all update

--- resources/views/layouts/partials-backend/sidebar-menu-tutor-left.blade.php
+++ resources/views/layouts/partials-backend/sidebar-menu-tutor-left.blade.php
@@ -27,6 +27,14 @@
                     </a>
                 </li>
                 <li class="">
+                    <a href="{{ route('tutor.dashboard') }}" class="svg-icon">
+                        <svg class="svg-icon" id="p-dash8" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path>
+                        </svg>
+                        <span class="ml-4">Estudiantes Asignados </span>
+                    </a>
+                </li>
+                <li class="">
                     <a href="#items" class="collapsed" data-toggle="collapse" aria-expanded="false">
                         <svg class="svg-icon" id="p-dash14" width="20" height="20"
                             xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor"
@@ -203,11 +211,21 @@
                                         <i class="las la-minus"></i><span>Pruebas Diagnóstico </span>
                                     </a>
                                 </li>
+
                             </ul>
                         </li>
                     </ul>
                 </li>
-
+                <li class="{{ request()->is('') ? 'active' : '' }}">
+                    <a href="https://fundacionmagistral.org/ayuda">
+                        <svg class="svg-icon" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <circle cx="12" cy="12" r="10"></circle>
+                            <path d="M9 9c0-1.5 1-2.5 2-2.5s2 1 2 2c0 1-1 1.5-1 2h-1"></path>
+                            <line x1="12" y1="16" x2="12" y2="16"></line>
+                        </svg>
+                        <span class="ml-4">Ayuda</span>
+                    </a>
+                </li>
 
 
 
