b7868abfbd8c7bb26af2d5f992af9811297f47ef
digilysolutions <digilysolutions@gmail.com>
2025-05-05 15:50:21 -0400

    Update sidebar-menu-user-left.blade.php

--- resources/views/layouts/partials-backend/sidebar-menu-user-left.blade.php
+++ resources/views/layouts/partials-backend/sidebar-menu-user-left.blade.php
@@ -108,7 +108,7 @@
                                     <i class="las la-minus"></i><span>Solicitar Tutor</span>
                                 </a>
                             </li>
-                          
+
 
 
                         </ul>
@@ -279,19 +279,7 @@
                     </li>
 
 
-                    <li class="{{ request()->is('study-centers*') ? 'active' : '' }}">
-                        <a href="{{ route('under.construction') }}">
-                            <svg class="svg-icon" width="20" height="20" viewBox="0 0 24 24" fill="none"
-                                stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round">
-                                <polyline points="6 9 6 2 18 2 18 9"></polyline>
-                                <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2">
-                                </path>
-                                <rect x="6" y="14" width="12" height="8"></rect>
-                            </svg>
-                            <span class="ml-4">Imprimir Reporte</span>
-                        </a>
-                    </li>
+
 
                 <li class="">
                     <a href="{{ route('under.construction') }}" class="svg-icon">
