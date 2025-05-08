f7cd70d455a9498ae42b5657177e39ae16667384
digilysolutions <digilysolutions@gmail.com>
2025-05-08 15:29:05 -0400

    Update dashboard.blade.php

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -7,7 +7,7 @@
     <div class="col-lg-12">
         <div class="card card-transparent card-block card-stretch card-height border-none">
             <div class="card-body p-3 mt-lg-2 mt-3">
-                <h3 class="mb-3">¡Bienvenido al Dashboard de Usuario!</h3>
+                <h3 class="mb-3">¡Bienvenido al Dashboard de Pedro Martínez!</h3>
                 <p class="mb-0 mr-4">Aquí podrás visualizar ¡Explora y aprende!</p>
             </div>
         </div>
@@ -17,9 +17,9 @@
                     $user = Auth::user();
                 @endphp
 
-                <h3> {{ $user->person->name }}</h3>
+                <h3> Pedro Martínez</h3>
 
-                <div class="alert text-white bg-info col-lg-12" role="alert">
+                <div class="alert text-black  col-lg-12" style="background-color: #e3e5e6;"  role="alert">
                     <div class="iq-alert-icon">
                         <i class="ri-information-line"></i>
                     </div>
@@ -38,12 +38,12 @@
             </div>
 
             <div class="row">
-                <div class="col-lg-3 col-md-3">
+                <div class="col-lg-3 col-md-3" >
                     <a href="{{ route('items.index', ['name' => 'Resueltos']) }}">
                         <div class="card card-block card-stretch card-height">
-                            <div class="card-body">
+                            <div class="card-body"  style="background-color:#f0f8ff ;">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
-                                    <div class="icon iq-icon-box-2 bg-info-light">
+                                    <div class="icon iq-icon-box-2" >
                                         <img src="{{ asset('img/47.jpg') }}" class="img-fluid" alt="image">
                                     </div>
                                     <div>
@@ -59,10 +59,10 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-3 col-md-3">
+                <div class="col-lg-3 col-md-3" >
                     <a href="{{ route('items.index', ['name' => 'No Resueltos']) }}">
                         <div class="card card-block card-stretch card-height">
-                            <div class="card-body">
+                            <div class="card-body"  style="background-color:#f0f8ff ;">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
                                     <div class="icon iq-icon-box-2 bg-danger-light">
                                         <img src="{{ asset('img/a4.png') }}" class="img-fluid" alt="image">
@@ -80,10 +80,10 @@
                         </div>
                     </a>
                 </div>
-                <div class="col-lg-3 col-md-3">
+                <div class="col-lg-3 col-md-3" >
                     <a href="{{ route('examens.index', ['name' => 'Examenes Resueltos']) }}">
                         <div class="card card-block card-stretch card-height">
-                            <div class="card-body">
+                            <div class="card-body"  style="background-color:#f0f8ff ;">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
                                     <div class="icon iq-icon-box-2 bg-success-light">
                                         <img src="{{ asset('img/3.png') }}" class="img-fluid" alt="image">
@@ -102,10 +102,10 @@
                     </a>
                 </div>
 
-                <div class="col-lg-3 col-md-3">
+                <div class="col-lg-3 col-md-3"  >
                     <a href="{{ route('examens.index', ['name' => 'Examenes No Resueltos']) }}">
                         <div class="card card-block card-stretch card-height">
-                            <div class="card-body">
+                            <div class="card-body" style="background-color:#f0f8ff ;">
                                 <div class="d-flex align-items-center mb-4 card-total-sale">
                                     <div class="icon iq-icon-box-2 bg-success-light">
                                         <img src="{{ asset('img/3.png') }}" class="img-fluid" alt="image">
