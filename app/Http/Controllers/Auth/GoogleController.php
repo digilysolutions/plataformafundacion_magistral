bc27418ec153a3d2c1c09816720ebcd011a94f29
digilysolutions <digilysolutions@gmail.com>
2025-05-13 14:11:51 -0400

    Update GoogleController.php

--- app/Http/Controllers/Auth/GoogleController.php
+++ app/Http/Controllers/Auth/GoogleController.php
@@ -29,11 +29,18 @@ class GoogleController extends Controller
             // Intenta obtener el usuario de Google
             $user = Socialite::driver('google')->user();
 
-            // Busca el usuario en la base de datos
+            // Busca en la base de datos un usuario con el mismo google_id
             $finduser = User::where('google_id', $user->id)->first();
 
+            // Verifica si ya existe un usuario con ese email
+            $userEmailExists = User::where('email', $user->email)->first();
+
             if ($finduser) {
+                // Usuario ya registrado con ese google_id
                 return redirect()->route('login')->with('status', "Ya tienes una cuenta registrada con Google. Por favor, inicia sesión para continuar.");
+            } elseif ($userEmailExists) {
+                // El correo ya está en uso por otro usuario
+                return redirect()->route('login')->with('error', "El correo electrónico {$user->email} ya está en uso por otra cuenta. Por favor, inicia sesión con esa cuenta o contacta al administrador.");
             } else {
                 DB::beginTransaction();
                 $password = Str::random(10);
@@ -44,8 +51,8 @@ class GoogleController extends Controller
                     'google_id' => $user->id,
                     'role' => 'Usuario',
                     'roleid' => 6,
-                    'membership_id'=>'BA0001',
-                    'password' => Hash::make( $password ), // Considera usar una mejor manera de manejar contraseñas
+                    'membership_id' => 'BA0001',
+                    'password' => Hash::make($password), // Considera usar una mejor manera de manejar contraseñas
                 ]);
                 $person = Person::create(
                     [
@@ -60,7 +67,7 @@ class GoogleController extends Controller
                         'name' => $user->name,
                         'activated' => true,
                         'people_id' => $person->id,
-                        'membership_id'=>'BA0001',
+                        'membership_id' => 'BA0001',
                     ]
                 );
                 Mail::to($user->email)->send(new SendEmailToGoogle($person, $password));
@@ -71,7 +78,7 @@ class GoogleController extends Controller
             DB::rollback();
             // Manejo de excepciones
             Log::error('Error de inicio de sesión con Google: ' . $e->getMessage());
-            return redirect()->route('login')->with('error', 'Algo salió mal durante el proceso de autenticación. Por favor, intenta nuevamente.'. $e->getMessage());
+            return redirect()->route('login')->with('error', 'Algo salió mal durante el proceso de autenticación. Por favor, intenta nuevamente.' . $e->getMessage());
         }
     }
 }
