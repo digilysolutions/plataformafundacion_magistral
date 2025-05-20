9a136211a074690170bc5000d236b3388ecc7687
digilysolutions <digilysolutions@gmail.com>
2025-05-20 03:04:27 -0400

    update all

--- resources/views/validator/dashboard.blade.php
+++ resources/views/validator/dashboard.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app-admin')
 @section('title-header-admin')
-Panel Validador - Plataforma Fundación Magistral
+    Panel Validador - Plataforma Fundación Magistral
 @endsection
 @section('content-admin')
 
     @php
         $user = Auth::user();
     @endphp
-   <h3>  {{$user->person->name}}</h3>
+    <h3> {{ $user->person->name }}</h3>
     @if (session('first_login'))
         <div class="iq-alert-text">
             <p>Usuario: {{ $user->email }}</p>
@@ -18,7 +18,7 @@ Panel Validador - Plataforma Fundación Magistral
         </div>
     @endif
     <div class="dashboard-content">
-        <h2>Área de Notificaciones</h2>
+        <h2>Notificaciones</h2>
         @if (isset($notifications) && count($notifications) > 0)
             <ul class="notifications-list">
                 @foreach ($notifications as $notification)
@@ -28,27 +28,49 @@ Panel Validador - Plataforma Fundación Magistral
                 @endforeach
             </ul>
         @else
-        <h4>Nuevos items por validar</h4>
-        <div class="card mb-12">
-            <div class="card-body">
+            <div class="dashboard-content">
 
 
-                    <div class="row">
-                            <div class="col-12 col-md-12 mb-12"> <!-- 12 para móviles, 4 para escritorio -->
-                                <div class="notification-item border p-3 rounded">
+                <div class="card mb-4">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-md-4 mb-3"> <!-- 12 para móviles, 4 para escritorio -->
+                                <div class="notification-item border p-3 rounded" style="background-color: #f0f8ff;">
+                                    <strong>ITEM Diagnóstico</strong><br>
                                     <strong>Código:</strong>MA-CMR-NUM-3RO-011<br>
-                                    <strong>Items:</strong> ¿Cuál es la forma correcta de escribir el número "cuatrocientos setenta y dos" usando cifras?<br>
+                                    <strong>Items:</strong> ¿Cuál es la forma correcta de escribir ....?<br>
                                     <strong>Especialidad:</strong>Matematica<br>
                                     <strong>Competencia:</strong>CMR-NUM<br>
-
+                                    <strong>Estado:</strong><span class="badge bg-warning-light">Por Validar</span><br>
                                     <em>Fecha: 08/05/2025</em>
                                 </div>
                             </div>
+                            <div class="col-12 col-md-4 mb-3"> <!-- 12 para móviles, 4 para escritorio -->
+                                <div class="notification-item border p-3 rounded" style="background-color: #f0f8ff;">
+                                    <strong>ITEM Diagnóstico</strong><br>
+                                    <strong>Código:</strong>MA-CMR-NUM-3RO-002<br>
+                                    <strong>Items:</strong> ¿Qué número completa correctamente...?<br>
+                                    <strong>Especialidad:</strong>Matematica<br>
+                                    <strong>Competencia:</strong>CMR-NUM<br>
+                                    <strong>Estado:</strong><span class="badge bg-primary">Validado</span><br>
+                                    <em>Fecha: 07/05/2025</em>
+                                </div>
+                            </div>
+                            <div class="col-12 col-md-4 mb-3"> <!-- 12 para móviles, 4 para escritorio -->
+                                <div class="notification-item border p-3 rounded" style="background-color: #f0f8ff;">
+                                    <strong>ITEM PISA</strong><br>
+                                    <strong>Código:</strong>MA-RAZ-N3-105<br>
+                                    <strong>Items:</strong> Una compañía de telefonía móvil ofrece...<br>
+                                    <strong>Especialidad:</strong>Matematica<br>
+                                    <strong>Nivel:</strong>3<br>
+                                    <strong>Estado:</strong><span class="badge bg-primary">Validado</span><br>
+                                    <em>Fecha: 07/05/2025</em>
+                                </div>
+                            </div>
+                        </div>
+
                     </div>
-
-            </div>
-        </div>
-
+                </div>
         @endif
     </div>
 
---
+++ resources/views/validator/personal_report.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app-admin')
+@section('title-header-admin')
+   Reporte Personal - Plataforma Fundación Magistral
+@endsection
+@section('content-admin')
+
+
+
+@endsection
